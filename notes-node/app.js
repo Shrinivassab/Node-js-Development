08afcf99adac1e69b2616cf7f4b8d1fb811a39c3
Shrinivassab <shrinivassab@gmail.com>
2018-07-11 11:35:40 -0400

    Third party modules

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,10 +2,17 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash'); // set of really handy utilities
 const notes = require('./notes.js');
 
+// console.log(_.isString(true));
+// console.log(_.isString('Shrinivass'));
+
+var filteredArray = _.uniq(['Shrinivass',1,1,2,3,4,"Shrinivass"]);
+console.log(filteredArray);
+
 var user = os.userInfo();
-console.log(user);
+// console.log(user);
 
 // fs.appendFile('greeting.txt','Hello' + user.username + '!', function (err) {
 //   if (err) {
@@ -13,8 +20,8 @@ console.log(user);
 //   }
 // });
 
-fs.appendFile('greeting.txt',`${user.username}`, function (err) {
-  if (err) {
-    console.log('Unable to write to file');
-  }
-});
+// fs.appendFile('greeting.txt',`${user.username}`, function (err) {
+//   if (err) {
+//     console.log('Unable to write to file');
+//   }
+// });
