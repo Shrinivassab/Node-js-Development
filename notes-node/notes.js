d2d87de7c10906c922f1968fa85bfc57a07f2117
Shrinivassab <shrinivassab@gmail.com>
2018-08-27 11:34:51 -0400

    Reading Notes and Resuablity

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -35,7 +35,9 @@ var getAll = () => {
 };
 
 var getNote = (title) => {
-  console.log('Getting note', title);
+  var notes = fetchNotes();
+  var selectedNotes = notes.filter((note) => note.title === title);
+  return (selectedNotes[0]);
 };
 
 var removeNote = (title) => {
@@ -50,9 +52,16 @@ var removeNote = (title) => {
   return notes.length !== filteredNotes.length;
 };
 
+var logNote = (note) => {
+  console.log('---');
+  console.log(`Title: ${note.title}`);
+  console.log(`Body: ${note.body}`);
+}
+
 module.exports = {
   addNote,
   getAll,
   getNote,
-  removeNote
+  removeNote,
+  logNote
 };
