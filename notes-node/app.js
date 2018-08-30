41390fc97bbea5f10e911afc81b666c650a38f0d
Shrinivassab <shrinivassab@gmail.com>
2018-08-30 14:52:26 -0400

    Listing Notes

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,5 +1,3 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
@@ -8,9 +6,6 @@ const notes = require('./notes.js');
 
 const argv = yargs.argv;
 var command = argv._[0];
-console.log('Command: ', command);
-console.log('Yargs', argv);
-
 if (command === 'add') {
   var note = notes.addNote(argv.title, argv.body);
   if (note) {
@@ -20,7 +15,11 @@ if (command === 'add') {
     console.log('Note title taken');
   }
 } else if (command === 'list') {
-  notes.getAll();
+  var allNotes = notes.getAll();
+  console.log(`Priting ${allNotes.length} note(s).`);
+  allNotes.forEach((note) => {
+    notes.logNote(note);
+  });
 } else if (command === 'read') {
   var note = notes.getNote(argv.title);
   if (note) {
