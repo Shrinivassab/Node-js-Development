80a5084316e27b05c1d37ec1c13bedf8a82ff7c2
Shrinivassab <shrinivassab@gmail.com>
2018-09-07 12:58:41 -0400

    encoding user input

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,29 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      aloas: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+  console.log(argv.a);
+  console.log(encodeURIComponent(argv.a));
+
+  const address = encodeURIComponent(argv.a);
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=7%20wallingford%20road%20brighton',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
   json: true
 }, (error, response, body) => {
-  // console.log(JSON.stringify(response, undefined, 2));
+  // console.log(JSON.stringify(body, undefined, 2));
   console.log(`Address:${body.results[0].formatted_address}`);
   console.log(`Lat:${body.results[0].geometry.location.lat}`);
   console.log(`Lat:${body.results[0].geometry.location.lng}`);
