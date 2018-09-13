c320b645c9f2d399ab437b2d63edcb81fab7a307
Shrinivassab <shrinivassab@gmail.com>
2018-09-13 12:39:41 -0400

    Wiring up weather search

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,37 +1,39 @@
+// const yargs = require('yargs');
+//
+// const geocode = require('./geocode/geocode');
+//
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Address to fetch weather for',
+//       string: true
+//     }
+//   })
+//   .help()
+//   .alias('help', 'h')
+//   .argv;
+//
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if (errorMessage) {
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
+
+// 839fae7d4f2bebe74e4daa13b3b2e9de
+
 const request = require('request');
-const yargs = require('yargs');
-
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      aloas: 'address',
-      describe: 'Address to fetch weather for',
-      string: true
-    }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
-
-  console.log(argv.a);
-  console.log(encodeURIComponent(argv.a));
-
-  const address = encodeURIComponent(argv.a);
 
 request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
+  url: 'https://api.darksky.net/forecast/839fae7d4f2bebe74e4daa13b3b2e9de/39.9350642,-75.1516194',
   json: true
 }, (error, response, body) => {
-  // console.log(JSON.stringify(body, undefined, 2));
-  if (error) {
-    console.log('Unable to connect to Google server.');
-  } else if (body.status === 'ZERO_RESULTS') {
-    console.log('Unable to find that address.');
-  } else if (body.status === 'OK') {
-    console.log(`Address:${body.results[0].formatted_address}`);
-    console.log(`Lat:${body.results[0].geometry.location.lat}`);
-    console.log(`Lat:${body.results[0].geometry.location.lng}`);
+  if (!error && response.statusCode === 200) {
+    console.log(body.currently.temperature);
+  } else {
+    console.log('Unable to fetch weather');
   }
-  console.log(body);
-})
+});
